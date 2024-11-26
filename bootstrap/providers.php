213448dc54a7fac1b06243594803929376d2262c
Subhankar Basak <subhankarbasakmail@gmail.com>
2024-11-27 00:23:10 +0530

    Added - some required packages

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+    Yajra\DataTables\DataTablesServiceProvider::class,
+    Milon\Barcode\BarcodeServiceProvider::class,
+    Barryvdh\DomPDF\ServiceProvider::class,
 ];
